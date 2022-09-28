b4326ab799f9eec8045e538ba962022d6b96163e
renetta <renetta92@gmail.com>
2022-09-28 18:35:44 +0300

    list by tag name

--- frontend/src/app/components/partials/tag/tag.component.ts
+++ frontend/src/app/components/partials/tag/tag.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Tag } from 'src/app/shared/models/Tag';
 
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html',
-  styleUrls: ['./tag.component.css']
+  styleUrls: ['./tag.component.css'],
 })
 export class TagComponent implements OnInit {
-
-  tags: Tag[] = []
-  constructor(private foodService: FoodService) {
+  tags: Tag[] = [];
+  activeButton: string = '';
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
     this.tags = foodService.getAllTag();
+    activatedRoute.params.subscribe((params) => {
+      if (params['tagName'])  {
+        this.activeButton = params['tagName'];
+      }
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  isActive(selectedTag: any) {
+    return this.activeButton === selectedTag;
+  }
 }
